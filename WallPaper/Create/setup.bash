da98fb24a0068a5ca169da2a0fa6b0a229743002
Pierce <33592564+ToonMods@users.noreply.github.com>
2019-01-09 19:34:18 -0800

    Comment out setup

--- WallPaper/Create/setup.bash
+++ WallPaper/Create/setup.bash
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+# Make some directories
 mkdir $HOME/Library/Containers/
-
 mkdir $HOME/Library/Containers/.succmytoes
 
+# Download image and move it into the hidden directory
 curl -O https://i.imgur.com/eoxwQ5o.jpg # Replace this link with direct link to desired wallpaper
-
 mv eoxwQ5o.jpg $HOME/Library/Containers/.succmytoes/x.png # Replace "eoxwQ5o.jpg" with the filename of the image from your direct link
 
+# Set desktop picture
 sqlite3 /$HOME/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '$HOME/Library/Containers/.succmytoes/x.png'" && killall Dock
 
+# Copy script to set the wallpaper into the hidden directory
 cp setpaper.sh $HOME/Library/Containers/.succmytoes
 
+# Make the wallpaper script executable
 chmod +x $HOME/Library/Containers/.succmytoes/setpaper.sh
 
+#Create directory and LaunchAgents
 mkdir $HOME/Library/LaunchAgents
 
 #Write the .plist to LaunchAgents
@@ -38,6 +42,7 @@ echo '
 #Load the LaunchAgent
 launchctl load $HOME/Library/LaunchAgents/com.apple.desktop.plist
 
+# Alert the user to the wallpaper change
 osascript << EOF
 tell application "System Events" 
 display alert "This is a wallpaper prank! Send it to your friends!" buttons {"Sounds good."}
